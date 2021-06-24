838f92021de7e76e203442dc6ddbd2bf30ca2e63
milandethariya <milandethariya.tech@gmail.com>
2021-06-24 09:16:09 +0530

    add ajax on  like & unlike

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -14,23 +14,30 @@ class LikesController < ApplicationController
   end
 
   def like
-    micropost = Micropost.find_by(id:params[:micropost_id])
-    if Like.where(user_id: current_user.id, micropost_id: micropost.id).count == 1
-      Like.where(user_id: current_user.id, micropost_id: micropost.id).update(is_like: true)
+    @micropost = Micropost.find_by(id: params[:id])
+    if Like.where(user_id: current_user.id, micropost_id: @micropost.id).count == 1
+      Like.where(user_id: current_user.id, micropost_id: @micropost.id).update(is_like: true)
     else
-      current_user.likes.create(micropost_id: micropost.id, is_like: true)
+      current_user.likes.create(micropost_id: @micropost.id, is_like: true)
     end
-    redirect_to timeline_user_path(current_user)
+    respond_to do |format|
+      format.html{ redirect_to timeline_user_path(current_user)}
+      format.js
+    end
   end
 
   def unlike
-    micropost = Micropost.find_by(id:params[:micropost_id])
-    if Like.where(user_id: current_user.id, micropost_id: micropost.id).count == 1
-      Like.where(user_id: current_user.id, micropost_id: micropost.id).update(is_like: false)
+    @micropost = Micropost.find_by(id: params[:id])
+    if Like.where(user_id: current_user.id, micropost_id: @micropost.id).count == 1
+      Like.where(user_id: current_user.id, micropost_id: @micropost.id).update(is_like: false)
     else
-      current_user.likes.create(micropost_id: micropost.id, is_like: false)
+      current_user.likes.create(micropost_id: @micropost.id, is_like: false)
     end
-    redirect_to timeline_user_path(current_user)
+    respond_to do |format|
+      format.html{ redirect_to timeline_user_path(current_user)}
+      format.js
+    end
+    
   end
 
 end
